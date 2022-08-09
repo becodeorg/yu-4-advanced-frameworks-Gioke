ddff415073f73e32df61d313be1492492e0e77a8
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-08-09 12:05:51 +0200

    Add logo to navbar

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './Images/Logo2.jpg';
 import './Header.css';
 
 export const NavBar = () => {
     return (
         <nav class="navbar">
+           <div className="logo">
+               <img src={ Logo} alt="logo"/>
+           </div>
             <ul class="ul-head">
                 <li class="li-head"><Link class="Link-head" to="#">Home</Link></li>
                 <li class="li-head"><Link class="Link-head" to="#">Content</Link></li>
