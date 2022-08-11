4dd9ed779f38623ae1bab786c5e711c23fa5c3f4
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-08-11 12:13:17 +0200

    change footer part2

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -6,12 +6,15 @@ export const Footer = () => {
     return(
         <footer className="bg-black flex justify-evenly h-auto">
             <div className="w-60">
-                <h4 className="text-white font-bold underline flex col-start-1 mt-2">Contacteer mij</h4>
+                <h4 className="text-white font-bold underline flex col-start-1 mt-4">Contacteer mij</h4>
                 <br />
                 <p className="text-white flex col-start-2">Problemen met je fiets of op zoek naar iets nieuws? <br/> Kom even langs ik help je graag verder:</p>
+                <div className="text-white mt-4 flex">
+                    <FaWhatsapp class="whatsapp mb-2 text-2xl mr-2" /> <FaInstagram class="instagram mb-2 text-2xl ml-2 mr-2"/> <FaFacebookF class="facebook mb-4 text-2xl ml-2"/>
+                </div>
             </div>
             <div className="mb-10">
-                <h4 className="text-white font-bold underline flex col-start-1 mt-2">Mijn diensten</h4>
+                <h4 className="text-white font-bold underline flex col-start-1 mt-4">Mijn diensten</h4>
                 <br />
                 <ul className="text-white">
                     <li><p className="text-white flex col-start-3">*Herstel fietsen</p></li>
@@ -22,12 +25,15 @@ export const Footer = () => {
                 </ul>
             </div>
             <div className="">
-                <h4 className="text-white font-bold underline flex col-start-1 mt-2">Mijn troeven</h4>
+                <h4 className="text-white font-bold underline flex col-start-1 mt-4">Mijn troeven</h4>
                 <br />
                 <ul>
                     <li><p className="text-white flex col-start-3">*snelle service</p></li>
                     <li><p className="text-white flex col-start-4">*kwaliteitsfietsen</p></li>
                 </ul>
+                <div className="flex col-end">
+                    <p className="text-gray-300 text-sm mt-20 mr-12">Copyright © Vélo-W  2022</p>
+                </div>
             </div>
 
         </footer>
@@ -35,10 +41,8 @@ export const Footer = () => {
 }
 
 
-// <div className="socials">
-//     <FaWhatsapp class="whatsapp"/> <FaInstagram class="instagram"/> <FaFacebookF class="facebook"/>
-// </div>
-// <p className="copyright">Vélo-W © 2022</p>
+
+
 // <ul className="ul-head">
 //     <li className="li-foot"><Link class="Link-foot" to="#">Home</Link></li>
 //     <li className="li-foot"><Link class="Link-foot" to="#">Gallery</Link></li>
