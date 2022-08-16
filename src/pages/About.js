d91c01ff75c4759ab67880af026d6d2da9b632fd
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-08-16 11:38:10 +0200

    Make icons in footer links

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import { Morgan, Deschee, Vdbparts, Shimano, Minerva, Dewo, Codagex, Verwimp, Ce
 
 export const About = () => {
     return (
-        <div className="h-screen">
+        <div className="">
         <div><h1 className="text-xl font-bold text-2xl underline flex justify-center mt-3">About Vélo-W:</h1>
             <br/>
             <p className="p-2">Hey.
@@ -23,19 +23,20 @@ export const About = () => {
             <div>
                 <h1 className="text-xl font-bold underline p-2">Partners:</h1>
             </div>
-            <div className="flex justify-evenly">
+            <div className="grid grid-cols-3 gap-2 md:grid-cols-1">
                 <a href="https://www.morganblue.net/" target="_blank"><img className="scale-50" src={ Morgan } alt="logo morgan blue"/></a>
                 <a href="https://www.descheemaeker.be/nl" target="_blank"><img className="scale-50" src={ Deschee } alt="Logo descheemaeker"/></a>
                 <a href="http://www.vdbparts.be/" target="_blank"> <img className="scale-50" src={ Vdbparts } alt="Logo VdbParts"/></a>
                 <a href="https://bike.shimano.com/nl-NL/home.html" target="_blank"><img className="scale-50" src={ Shimano } alt="Logo Shimano"/></a>
-            </div>
-            <div className="flex justify-evenly">
                 <a href="https://minerva-cycling.be/nl/" target="_blank"><img className="w-60 h-28" src={ Minerva } alt="Logo Minerva"/></a>
                 <a href="https://www.dewo-europe.com/" target="_blank"><img className="scale-50" src={ Dewo } alt=" Logo Dewo-Europe"/></a>
                 <a href="https://codagex.be/websolutions/default.aspx" target="_blank"><img className="scale-50" src={ Codagex } alt="Logo Codagex"/></a>
-                <a href="https://www.verwimp.nl/nl" target="_blank"><img className="scale-50" src={ Verwimp } alt="Logo Verwimp"/></a>
+                <a href="https://www.verwimp.nl/nl" target="_blank"><img className="scale-90" src={ Verwimp } alt="Logo Verwimp"/></a>
                 <a href="https://www.cemabearing.be/index.php?route=common/home" target="_blank"><img className="scale-50" src={ Cema } alt="Logo Cema Bearing"/></a>
             </div>
+            {/*<div className="flex flex-wrap">*/}
+
+            {/*</div>*/}
         </div>
 
     )
